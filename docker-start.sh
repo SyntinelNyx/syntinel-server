728829f7f390f88f770a9fddc98812fd0b4e1e68
SapphireGaze <jasminehuang2048@outlook.com>
2025-03-02 18:58:58 -0800

    A Rubber Room

--- docker-start.sh
+++ docker-start.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 mkdir -p /app/data
+mkdir -p /app/data/certs
 
-# List of required files
-FILES="ca_cert.pem ca_key.pem config.yaml ecdsa_private.pem ecdsa_public.pem server_cert.pem server_key.pem"
+FILES = "config.yaml ecdsa_private.pem ecdsa_public.pem"
+CERTS="ca_cert.pem ca_key.pem server_cert.pem server_key.pem"
 
 for file in $FILES; do
     if [ ! -f "/app/data/$file" ]; then
         cp "/app/defaults/$file" "/app/data/$file"
+    fi
+done
+
+for cert in $CERTS; do
+    if [ ! -f "/app/data/$cert" ]; then
+        cp "/app/defaults/$cert" "/app/data/certs/$cert"
     fi
 done
 
