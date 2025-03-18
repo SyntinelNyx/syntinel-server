801494f4a722643d7a02204fad5202a9e3d99ba4
rogueking <miguel@muniz.org>
2025-03-17 22:14:50 -0700

    communication functional

--- Makefile
+++ Makefile
@@ -14,4 +14,4 @@ clean:
 proto:
 	protoc --go_out=./ --go_opt=paths=source_relative \
 	--go-grpc_out=./ --go-grpc_opt=paths=source_relative \
-	./internal/proto/hardware_info.proto
+	./internal/proto/bidirectional_comm.proto
--- internal/grpc/grpc.go
+++ internal/grpc/grpc.go
@@ -2,7 +2,10 @@ package grpc
 
 import (
 	"context"
+	"io"
+	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 
@@ -11,12 +14,12 @@ import (
 )
 
 type server struct {
-	proto.UnimplementedHardwareServiceServer
+	proto.UnimplementedAgentServiceServer
 }
 
-func (s *server) ReceiveHardwareInfo(ctx context.Context, req *proto.HardwareInfo) (*proto.Response, error) {
+func (s *server) SendHardwareInfo(ctx context.Context, req *proto.HardwareInfo) (*proto.HardwareResponse, error) {
 	logger.Info("Received hardware info: %s", req.JsonData)
-	return &proto.Response{Message: "Hardware info received successfully"}, nil
+	return &proto.HardwareResponse{Message: "Hardware info received successfully"}, nil
 }
 
 func StartServer(grpcServer *grpc.Server) *grpc.Server {
@@ -25,7 +28,7 @@ func StartServer(grpcServer *grpc.Server) *grpc.Server {
 		logger.Fatal("Failed to listen: %v", err)
 	}
 
-	proto.RegisterHardwareServiceServer(grpcServer, &server{})
+	proto.RegisterAgentServiceServer(grpcServer, &server{})
 
 	logger.Info("gRPC server listening on :50051 with TLS...")
 	if err := grpcServer.Serve(listener); err != nil {
@@ -33,4 +36,41 @@ func StartServer(grpcServer *grpc.Server) *grpc.Server {
 	}
 
 	return grpcServer
+}
+
+func (s *server)BidirectionalStream(stream proto.AgentService_BidirectionalStreamServer) error {
+	go func() {
+		for {
+			req, err := stream.Recv()
+			if err == io.EOF {
+				log.Println("Agent closed the stream")
+				break
+			}
+			if err != nil {
+				log.Printf("Error receiving message from agent: %v", err)
+				break
+			}
+			log.Printf("Received message from agent: %s", req.Name)
+		}
+	}()
+
+	// Example: Sending a file to the agent
+	filePath := "./data/scripts/example.txt"
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		return err
+	}
+
+	err = stream.Send(&proto.ScriptResponse{
+		Name:    "example.txt",
+		Status:  "File sent successfully",
+		Content: string(content),
+	})
+	if err != nil {
+		log.Printf("Error sending file to agent: %v", err)
+		return err
+	}
+
+	return nil
 }
