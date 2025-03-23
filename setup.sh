965026b81523ac0d8dc141b992ad4ba7d90c6abe
rogueking <miguel@muniz.org>
2025-03-23 02:17:43 -0700

    cleanup

--- setup.sh
+++ setup.sh
@@ -28,8 +28,8 @@ openssl req -new                                    \
   -reqexts test_server
 
 # Change permissions of the server key and csr to allow postgre to read them
-chmod 600 data/server_key.pem 
-chmod 600 data/server_csr.pem
+#chmod 600 data/server_key.pem 
+#chmod 600 data/server_csr.pem
 
 # Sign the server certificate with the CA.
 openssl x509 -req           \
@@ -44,7 +44,7 @@ openssl x509 -req           \
   -sha256
 
 # Change permissions of the server key and csr to allow postgre to read them
-chmod 600 data/server_cert.pem
+#chmod 600 data/server_cert.pem
 
 # Verify the generated server certificate.
 openssl verify -verbose -CAfile data/ca_cert.pem data/server_cert.pem
