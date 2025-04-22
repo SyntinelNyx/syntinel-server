b2c5e741d7a9c1cd387d70d9d9c020aa1f3c5e97
Christian <christianunr@gmail.com>
2025-04-22 09:56:28 -0700

    Fixed auth query to work with middleware properly.

--- internal/database/postgresql/queries/auth.sql
+++ internal/database/postgresql/queries/auth.sql
@@ -2,7 +2,7 @@
 INSERT INTO root_accounts (
   email, username, password_hash, created_at, updated_at, email_verified_at
 ) VALUES (
-  $1, $2, $3, EXTRACT(EPOCH FROM NOW()), EXTRACT(EPOCH FROM NOW()), NULL
+  $1, $2, $3, NOW(), NOW(), NULL
 )
 RETURNING *;
 
