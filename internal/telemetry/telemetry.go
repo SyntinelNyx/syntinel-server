ddf1119f45a7a04345153507049070ccf334226a
rogueking <miguel@muniz.org>
2025-04-27 17:07:18 -0700

    added telemetry runner

--- internal/telemetry/telemetry.go
+++ internal/telemetry/telemetry.go
@@ -2,10 +2,14 @@ package telemetry
 
 import (
 	"context"
-	"net/http"
+	"encoding/json"
+	"time"
 
-	"github.com/SyntinelNyx/syntinel-server/internal/auth"
-	"github.com/SyntinelNyx/syntinel-server/internal/response"
+	"github.com/SyntinelNyx/syntinel-server/internal/commands"
+	"github.com/SyntinelNyx/syntinel-server/internal/database/query"
+	"github.com/SyntinelNyx/syntinel-server/internal/logger"
+	"github.com/SyntinelNyx/syntinel-server/internal/proto/controlpb"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
@@ -39,22 +43,104 @@ func (h *Handler) telemetryRunner() {
 	var memory Memory
 	var disk Disk
 
-	var rootID pgtype.UUID
-	var assetID pgtype.UUID
-	var err error
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+	ctx := context.Background()
 
-	account := auth.GetClaims(r.Context())
-	if account.AccountType != "root" {
-		rootID, err = h.queries.GetRootAccountIDForIAMUser(context.Background(), account.AccountID)
-		if err != nil {
-			response.RespondWithError(w, r, http.StatusInternalServerError, "Failed to get associated root account for IAM account", err)
-			return
+	// Endless loop with ticker
+	for {
+		select {
+		case <-ticker.C:
+			// Get all assets to collect telemetry from
+			assets, err := h.queries.GetAllAssets(ctx, pgtype.UUID{})
+			if err != nil {
+				logger.Error("Failed to get assets for telemetry: %v", err)
+				continue
+			}
+
+			for _, asset := range assets {
+				// Skip assets without IP
+				if asset.IpAddress.IsValid() == false {
+					continue
+				}
+
+				// Prepare control message for sysinfo command
+				controlMessages := []*controlpb.ControlMessage{
+					{
+						Command: "exec",
+						Payload: "sysinfo",
+					},
+				}
+
+				// Execute sysinfo command on the asset
+				responses, err := commands.Command(asset.IpAddress.String(), controlMessages)
+				if err != nil {
+					logger.Error("Failed to execute sysinfo on %s: %v", asset.IpAddress.String(), err)
+					continue
+				}
+
+				// Parse the response
+				if len(responses) > 0 {
+					var sysinfo SysInfo
+					if err := json.Unmarshal([]byte(responses[0]), &sysinfo); err != nil {
+						logger.Error("Failed to parse sysinfo response from %s: %v", asset.IpAddress.String(), err)
+						continue
+					}
+
+					// Get root account ID for this asset
+					var rootID pgtype.UUID
+					rootID.Scan(asset.RootAccountID)
+
+					// Insert telemetry data into database
+					telemetryID := pgtype.UUID{}
+					err = telemetryID.Scan(uuid.New())
+					if err != nil {
+						logger.Error("Failed to generate UUID: %v", err)
+						continue
+					}
+
+					telemetryTime := pgtype.Timestamptz{}
+					err = telemetryTime.Scan(time.Now())
+					if err != nil {
+						logger.Error("Failed to generate timestamp: %v", err)
+						continue
+					}
+
+					assetID := pgtype.UUID{}
+					err = assetID.Scan(asset.AssetID)
+					if err != nil {
+						logger.Error("Failed to parse asset ID: %v", err)
+						continue
+					}
+
+					// Prepare parameters for database insertion
+					params := query.InsertTelemetryDataParams{
+						TelemetryID:     telemetryID,
+						TelemetryTime:   telemetryTime,
+						CpuUsage:        sysinfo.CpuUsage,
+						MemTotal:        int64(sysinfo.MemUsage.Total),
+						MemAvailable:    int64(sysinfo.MemUsage.Available),
+						MemUsed:         int64(sysinfo.MemUsage.Used),
+						MemUsedPercent:  sysinfo.MemUsage.UsedPercent,
+						DiskTotal:       int64(sysinfo.DiskUsage.Total),
+						DiskFree:        int64(sysinfo.DiskUsage.Free),
+						DiskUsed:        int64(sysinfo.DiskUsage.Used),
+						DiskUsedPercent: sysinfo.DiskUsage.UsedPercent,
+						AssetID:         assetID,
+						RootAccountID:   rootID,
+					}
+
+					// Insert the telemetry data
+					_, err = queries.InsertTelemetryData(ctx, params)
+					if err != nil {
+						logger.Error("Failed to insert telemetry data for %s: %v", asset.IpAddress.String(), err)
+						continue
+					}
+
+					logger.Info("Telemetry data collected from %s", asset.IpAddress.String())
+				}
+			}
 		}
-	} else {
-		rootID = account.AccountID
 	}
-
-	
-	
 
 }
